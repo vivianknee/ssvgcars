53789db5ecb97a9f457696158120c98bb9fa13b3
Vivian <vivianni452@gmail.com>
2023-03-01 11:14:00 -0800

    adding carspec api and deleting jkes and covid

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
-from model.jokes import initJokes
 from model.users import initUsers
 from model.cars import initCars
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.car import car_api # Blueprint import api definition
 
@@ -19,8 +16,6 @@ from api.car import car_api # Blueprint import api definition
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(car_api) # register api routes
@@ -41,7 +36,6 @@ def stub():
 @app.before_first_request
 def activate_job():  # activate these items 
     db.init_app(app)
-    initJokes()
     initUsers()
     initCars()
 
